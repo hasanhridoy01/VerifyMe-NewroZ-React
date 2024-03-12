d136d9beabe4ea6f5ee62ff0a7c7259bcfa45f01
hasanhridoy01 <hasanhridoy1367@gmail.com>
2024-03-12 15:34:45 -0700

    header section account icon done

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -130,7 +130,7 @@ const Header = () => {
 
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0, background: '#ffffff' }}>
                 <Avatar alt="Remy Sharp" src="../../../../public/images/header/Frame 48095401.png" />
               </IconButton>
             </Tooltip>
--- src/components/layout/Main/Main.jsx
+++ src/components/layout/Main/Main.jsx
@@ -1,14 +1,14 @@
 import { Outlet } from "react-router-dom";
-import Header from '../../common/Header/Header';
-import './Main.css'
+import Header from "../../common/Header/Header";
+import "./Main.css";
 
 const Main = () => {
-    return (
-        <div>
-            <Header></Header>
-            <Outlet></Outlet>
-        </div>
-    );
+  return (
+    <div style={{ padding: "15px 10px 10px 10px" }}>
+      <Header></Header>
+      <Outlet></Outlet>
+    </div>
+  );
 };
 
 export default Main;
