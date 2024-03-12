dfeef37590f60ea56a844878f159c5921d1c791f
hasanhridoy01 <hasanhridoy1367@gmail.com>
2024-03-12 12:58:35 -0700

    router setup done

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Main from './components/layout/Main/Main.jsx';
+import Header from './components/common/Header/Header.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Header></Header>
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>,
+)
